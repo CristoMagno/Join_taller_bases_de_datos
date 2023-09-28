a688093c22c03659d6fad4d2b371f5e586f80049
CRISTO MAGNO <127151280+CristoMagno@users.noreply.github.com>
2023-09-28 16:33:09 -0600

    Update APEX.sql

--- APEX.sql
+++ APEX.sql
@@ -4,3 +4,33 @@ Left join
 Right join
 Full join
 */
+select * from crimes;
+SELECT suspect_id, name, crime_id, crime_type,  committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE name = 'Kasper Good';
+
+SELECT suspect_id, name, crime_id, crime_type,  committed_date, prison, start_date, end_date, behavior FROM suspects s right join crimes c on WHERE name = 'Kasper Good';
+/*
+Natural
+Inner
+Right
+Left
+Full
+*/
+select * from suspects where suspect_id = 338;
+
+
+
+
+
+SELECT name, crime_id, crime_type, committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE suspect_id = 338;
+
+
+SELECT suspect_id FROM suspects  WHERE name = 'Hilary Mayer' AND hair_color = 'Red'  AND eye_color= 'Blue' AND feet_size= 'Small' AND tattoos = 'Yes' AND age BETWEEN 35 AND 40;
+
+
+
+SELECT name, crime_id, crime_type, committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE suspect_id = 100;
+
+
+SELECT crime_id, crime_type, committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE suspect_id = 100 ORDER BY committed_date;
+
+SELECT crime_id, crime_type, committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE suspect_id = 100 ORDER BY committed_date DESC;
