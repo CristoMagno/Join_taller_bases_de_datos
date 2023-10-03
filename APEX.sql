55fc1922c97c76dadc2c84aaa6d10f457868859c
CRISTO MAGNO <127151280+CristoMagno@users.noreply.github.com>
2023-10-03 10:05:06 -0600

    Update APEX.sql

--- APEX.sql
+++ APEX.sql
@@ -35,7 +35,21 @@ SELECT crime_id, crime_type, committed_date, prison, start_date, end_date, behav
 
 SELECT crime_id, crime_type, committed_date, prison, start_date, end_date, behavior FROM suspects NATURAL JOIN crimes WHERE suspect_id = 100 ORDER BY committed_date DESC;
 
+
+/*nuevas consultas*/
 Select suspect_id, name, crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL join crimes WHERE name = 'Kasper Good';
 Select name crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL JOIN crimes where suspect_id = 338;
 Select name, crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL JOIN crimes WHERE suspect_id = 100;
 Select crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL JOIN crimes WHERE suspect_id = 100 ORDER BY committed_date;
+
+Select crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL JOIN crimes WHERE suspect_id = 100 ORDER BY committed_date desc;
+Select name, crime_id, crime_type, committed_date, prison, start_date, end_date, behavior from suspects NATURAL JOIN crimes WHERE suspect_id = 338 ORDER BY end_date desc;
+Select name, hair_color, eye_color, crime_type, prison, start_date, end_date, behavior from suspects NATURAL join crimes WHERE suspect_id = 20 ORDER BY committed_date;
+
+Select name, prison, crime_type from suspects NATURAL JOIN crimes WHERE suspect_id = 59 ORDER BY crime_type;
+Select name, suspect_id, crime_type, committed_date FROM suspects NATURAL JOIN crimes WHERE sex = 'Female' AND hair_color = 'Red' AND glasses = 'Yes' AND scars = 'No' AND feet_size = 'Small' AND crime_type = 'Car Theft' ORDER BY committed_date DESC;
+Select name, crime_type, COUNT(crime_type) from suspects NATURAL join crimes where suspect_id = 400 and crime_type = 'Car Theft' GROUP BY name, crime_type;
+Select * from suspects natural JOIN crimes where sex = 'Male' and hair_color = 'Blonde' and scars = 'Yes' and glasses = 'Yes' and facial_hair = 'No' and tattoos = 'Yes' ORDER BY crime_type;
+Select name, crime_type, COUNT(crime_type) from suspects natural join crimes where sex = 'Male' and hair_color = 'Blonde' and scars = 'Yes' and glasses = 'Yes' and facial_hair = 'No' and tattoos = 'Yes' GROUP BY name, crime_type ORDER BY COUNT(crime_type) DESC;
+
+/**/
